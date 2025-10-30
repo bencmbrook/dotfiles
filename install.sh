33bf9299d838707b2ce8bae0a91c60d9a410e320
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-10-30 18:22:26 -0400

    update install script

--- install.sh
+++ install.sh
@@ -3,7 +3,23 @@ set -e
 
 # Install homebrew if not already installed
 if ! command -v brew &> /dev/null; then
+  # Get the Homebrew prefix (https://github.com/Homebrew/install/blob/dfeeaedef9962e643e0190747c576ef36ac111ef/install.sh#L166-L186)
+  if [[ "$(uname)" == "Linux" ]]; then
+    HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+  elif [[ "$(uname)" == "Darwin" ]]; then
+    HOMEBREW_PREFIX="/opt/homebrew"
+  else
+    echo "Unsupported platform: $(uname)"
+    exit 1
+  fi
+
+  # Install homebrew
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Add homebrew to the PATH
+  echo >> ${ZDOTDIR:-$HOME}/.zshrc
+  echo "eval \"\$(${HOMEBREW_PREFIX}/bin/brew shellenv)\"" >> ${ZDOTDIR:-$HOME}/.zshrc
+  eval "$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
 fi
 
 # Install starship and antidote
@@ -21,5 +37,4 @@ if ! grep -q "source $ZSHRC_PATH" "${ZDOTDIR:-$HOME}/.zshrc"; then
   echo "source $ZSHRC_PATH" | cat - "${ZDOTDIR:-$HOME}/.zshrc" > "${ZDOTDIR:-$HOME}/.zshrc.tmp" && mv "${ZDOTDIR:-$HOME}/.zshrc.tmp" "${ZDOTDIR:-$HOME}/.zshrc"
 fi
 
-source ${ZDOTDIR:-$HOME}/.zshrc
 echo "Done! ${ZDOTDIR:-$HOME}/.zshrc has been updated to source $ZSHRC_PATH"
